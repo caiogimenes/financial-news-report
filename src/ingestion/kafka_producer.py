57116758a12249e1a2024cf79a8a86d2610bd3e9
caiogimenes <ciaonemegi@gmail.com>
2025-08-11 22:55:41 -0300

    feat: refactor KafkaProducer to use config for bootstrap servers and enhance data handling

--- src/ingestion/kafka_producer.py
+++ src/ingestion/kafka_producer.py
@@ -1,16 +1,24 @@
 from confluent_kafka import Producer
+from config import config
+
 
 class KafkaProducer:
-    def __init__(self, bootstrap_servers, topic):
+    def __init__(self, topic):
         self.producer = Producer({
-            'bootstrap.servers': bootstrap_servers,
-            'client.id': 'kafka_producer'
+            'bootstrap.servers': config['kafka']['KAFKA_BOOTSTRAP_SERVERS'],
+            'security.protocol': config['kafka']['security.protocol'],
+            'sasl.mechanisms': config['kafka']['sasl.mechanisms'],
+            'sasl.username': config['kafka']['sasl.username'],
+            'sasl.password': config['kafka']['sasl.password'],
+            'client.id': config['kafka']['client.id'],
         })
-        self.topic = topic
+        self.topic = 'raw_data' if not topic else topic
 
-    def send_message(self, message):
-        self.producer.produce(self.topic, value=message.encode('utf-8'))
+    def send_data(self, data):
+        if not isinstance(data, str):
+            try:
+                data = str(data)
+            except ValueError as e:
+                raise ValueError(f"{e}. Check if data is string type")
+        self.producer.produce(self.topic, value=data.encode('utf-8'))
         self.producer.flush()
-
-    def close(self):
-        self.producer.close()
