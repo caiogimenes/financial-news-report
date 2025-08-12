781de2a961fc53c8c03162a5995188694b8eec7a
caiogimenes <ciaonemegi@gmail.com>
2025-08-11 23:03:49 -0300

    feat: add configuration for Finnhub API and Kafka connection settings

---
+++ src/processing/config.py
@@ -0,0 +1,12 @@
+config = {
+    "FINNHUB_API_KEY": "d2bkv1pr01qvh3vc94hgd2bkv1pr01qvh3vc94i0",
+    'kafka': {
+        'KAFKA_BOOTSTRAP_SERVERS': 'pkc-p11xm.us-east-1.aws.confluent.cloud:9092',
+        'security.protocol': 'SASL_SSL',
+        'sasl.mechanisms': 'PLAIN',
+        'sasl.username': 'F37GT4XKKXJBHEB2',
+        'sasl.password': 'cflt41gl/PVZhGODfCAgGY37yuGEKiQmF9aAddAdXpIK9SVryNFWSbE4TNBVC6lg',
+        "client.id": "ccloud-python-client-1c103f18-0d03-4cbd-aae9-c8e2cb956593"
+    }
+
+}
